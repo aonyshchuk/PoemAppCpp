2583d2e5c684d834df9fa3dc40e6c1455988795b
aonyshchuk <alinaonyshchuk7@gmail.com>
2022-10-03 06:42:24 +0300

    Done task 1

--- Main.cpp
+++ Main.cpp
@@ -1,8 +1,23 @@
+/*Завдання 1. Створити додаток, який виводить інформацію про книгу, у такому форматі:
+Name : “The war and the peace”
+Avtor : L.N. Tolstoj
+Izdatelstvo: Piter
+Pages : 500.*/
+
+
+
+
 #include <iostream>
 using namespace std;
 
 int main()
 {
+
+		cout << "Name : \"The war and the peace\"" << endl;
+		cout << "Avtor : L.N. Tolstoj" << endl;
+		cout << "Izdatelstvo: Piter" << endl;
+		cout << "Pages : 500." << endl;
+
 
 	return 0;
 }
