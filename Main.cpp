75552a800ecd28d64aaedc3406809d1a34c9dccf
aonyshchuk <alinaonyshchuk7@gmail.com>
2022-10-04 23:10:41 +0300

    Done task 2

--- Main.cpp
+++ Main.cpp
@@ -1,8 +1,17 @@
+/* Завдання 2. Створити додаток, який виводить на консоль фразу
+ Супроводити кожен новий рядок потрійним звуковим сигналом
+*/
 #include <iostream>
+
+
 using namespace std;
 
 int main()
 {
+	cout << "\"Kraj lukomor’a dub zelenyj,\a" << endl; 
+	cout << "zolotyj lancyuh na nim:	  " << endl; 
+	cout << "hodnya, shonochi kit uchenyj " << endl; 
+	cout << "a lancyuhu kruzhl’aje tim\"  " << endl; 
 
 	return 0;
 }
